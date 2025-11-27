69987563d9915ac00c6eb4f3d67dab57a2958c4a
ErikBerik <erik.j.berglund@hotmail.com>
2025-11-27 18:07:10 +0100

    Users now provide a promput upon running the program

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -10,10 +11,16 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    content_response = client.models.generate_content(model="gemini-2.0-flash-001",contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(content_response.text)
-    print(f"Prompt tokens: {content_response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {content_response.usage_metadata.candidates_token_count}")
+    argument_list = sys.argv
+    if len(argument_list) < 2:
+        print("Please provide a string as an argument to this program")
+        sys.exit(1)
+    else:
+        query_string = argument_list[1]
+        content_response = client.models.generate_content(model="gemini-2.0-flash-001",contents=query_string)
+        print(content_response.text)
+        print(f"Prompt tokens: {content_response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {content_response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
