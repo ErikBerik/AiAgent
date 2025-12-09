0476852a9a75b6b846df6c09d41b5d01cd090e69
ErikBerik <erik.j.berglund@hotmail.com>
2025-12-09 16:22:56 +0100

    Refactored some code to reflect course updates since I last worked on it. Among others: Using argparse to handle user input

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -12,19 +12,18 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    argument_list = sys.argv
-    if len(argument_list) < 2:
-        print("Please provide a string as an argument to this program")
-        sys.exit(1)
-    else:
-        user_prompt = argument_list[1]
-        messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-        ]
-        content_response = client.models.generate_content(model="gemini-2.0-flash-001",contents=messages)
-        print(content_response.text)
-        print(f"Prompt tokens: {content_response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {content_response.usage_metadata.candidates_token_count}")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    user_prompt = args.user_prompt
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    content_response = client.models.generate_content(model="gemini-2.5-flash",contents=messages)
+    print(content_response.text)
+    print(f"Prompt tokens: {content_response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {content_response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
